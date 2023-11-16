7ec00e55791d808589552d0f3f6cfaa2416eef56
prashantdelhi1995 <00udemy00@gmail.com>
2023-11-16 15:03:44 +0530

    add new project

---
+++ index.js
@@ -0,0 +1,8 @@
+var express= require ("express");
+var app=express();
+app.get("/" , (req,res)=> {
+    res.send("hello world");
+})
+app.listen(3000, ()=>{
+    console.log(`server is running on port 3000`)
+})
